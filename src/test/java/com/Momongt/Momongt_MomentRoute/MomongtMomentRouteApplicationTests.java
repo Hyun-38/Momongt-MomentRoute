5dd9c4186b477d06647253c013cdc4bb87cbbf9e
Hyun-38 <1226johnny@gmail.com>
2025-10-26 17:55:07 +0900

    Momongt Backend 초기 설정

---
+++ src/test/java/com/Momongt/Momongt_MomentRoute/MomongtMomentRouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Momongt.Momongt_MomentRoute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomongtMomentRouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
